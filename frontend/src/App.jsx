dead87b24641faba9987741165afaaf9fde65d1c
jong-seoung <jjong015189@gmail.com>
2025-10-16 17:51:06 +0900

    frontend: profile & profile edit & profile image edit #5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -40,6 +40,12 @@ const App = () => {
             isAuthenticated ? <Navigate to="/" /> : <EmailVerify replace />
           }
         />
+        <Route
+          path="/profile/:userId"
+          element={
+            isAuthenticated ? <Profile /> : <Navigate to="/login" replace />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
